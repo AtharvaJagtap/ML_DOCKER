f6d2e118a0127a6a88e611d7846a9ae2a86ae6a0
atharva <atharva@localhost.localdomain>
2021-05-29 16:21:28 -0400

    initial commit

---
+++ script.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+
+import joblib
+
+
+model = joblib.load("Salary.pkl")
+
+data = float(input("Enter your experience:"))
+prediction = model.predict([[data]])
+print(f"You will receive Rs {prediction[0]} approximately...")
